8e878c634403f5918219ec05eb74d8321f964d07
Jordan Hutchings <jordanhutch04@gmail.com>
2023-08-23 17:39:00 -0400

    move to quarto

---
+++ blog/2023-08-01-strava-api-viz/secrets.R
@@ -0,0 +1,4 @@
+app_name = 'wahoo-climb-2022' # chosen by user
+app_client_id  = '77075' # an integer, assigned by Strava
+app_secret = 'e932547d81f6702c3a69d2eeb6ab7b08029b1f05' # an alphanumeric secret, assigned by Strava
+strava_id = "60832391"
